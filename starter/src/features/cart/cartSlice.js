76551b49b759c4c974ebead3ce30c00d6ae4ab18
Ismail KHAIRY <tawazlwb@gmail.com>
2022-10-26 17:32:06 +0200

    416. Reducers - Calculate Totals

--- starter/src/features/cart/cartSlice.js
+++ starter/src/features/cart/cartSlice.js
@@ -3,7 +3,7 @@ import cartItems from '../../cartItems'
 
 const initialState = {
   cartItems: cartItems,
-  amount: 4,
+  amount: 0,
   total: 0,
   isLoading: true,
 }
@@ -27,11 +27,27 @@ const cartSlice = createSlice({
       const cartItem = state.cartItems.find((item) => item.id === payload.id)
       cartItem.amount = cartItem.amount - 1
     },
+    calculateTotals: (state) => {
+      const { total, amount } = state.cartItems.reduce(
+        (previousValue, currentItem) => {
+          const newTotal =
+            previousValue.total + currentItem.amount * currentItem.price
+          const newAmount = previousValue.amount + currentItem.amount
+
+          return { total: newTotal, amount: newAmount }
+        },
+        { total: 0, amount: 0 }
+      )
+
+      state.total = total
+      state.amount = amount
+    },
   },
 })
 
 // console.log(cartSlice)
 
-export const { clearCart, removeItem, increase, decrease } = cartSlice.actions
+export const { clearCart, removeItem, increase, decrease, calculateTotals } =
+  cartSlice.actions
 
 export default cartSlice.reducer
