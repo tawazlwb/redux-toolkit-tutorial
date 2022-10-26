13277d9e16180fe6d66750cd2263dad2d7dc2f24
Ismail KHAIRY <tawazlwb@gmail.com>
2022-10-26 16:36:19 +0200

    411. Cart Item

---
+++ starter/src/components/CartItem.js
@@ -0,0 +1,28 @@
+import { useSelector } from 'react-redux'
+import { ChevronDown, ChevronUp } from '../icons'
+
+export const CartItem = ({ id, title, price, img, amount }) => {
+  // const { amount } = useSelector((store) => store.cart)
+
+  return (
+    <article className='cart-item'>
+      <img src={img} alt={title} />
+      <div>
+        <h4>{title}</h4>
+        <h4 className='item-price'>${price}</h4>
+        <button className='remove-btn'>remove</button>
+      </div>
+      <div>
+        <button className='amount-btn'>
+          <ChevronUp />
+        </button>
+        <p className='amount'>{amount}</p>
+        <button className='amount-btn'>
+          <ChevronDown />
+        </button>
+      </div>
+    </article>
+  )
+}
+
+export default CartItem
